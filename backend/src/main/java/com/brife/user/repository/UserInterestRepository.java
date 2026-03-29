34e4ae45db17b85376c46380e894d243a05fea6e
Minwoo-lee-lee732 <lee732@uwm.edu>
2026-03-29 21:55:04 +0900

    [back] fix: 관심사 재설정 중복키 오류 수정

--- backend/src/main/java/com/brife/user/repository/UserInterestRepository.java
+++ backend/src/main/java/com/brife/user/repository/UserInterestRepository.java
@@ -3,6 +3,8 @@ package com.brife.user.repository;
 import com.brife.user.domain.UserInterest;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -11,6 +13,8 @@ public interface UserInterestRepository extends JpaRepository<UserInterest, Long
     @EntityGraph(attributePaths = {"category", "categoryGroup"})
     List<UserInterest> findByUserId(Long userId);
 
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("DELETE FROM UserInterest ui WHERE ui.user.id = :userId")
     void deleteByUserId(Long userId);
 
     boolean existsByUserId(Long userId);
