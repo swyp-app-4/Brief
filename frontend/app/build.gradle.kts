2fcf1296eafff861f8b44819e8e5ecee2e1670af
효정 <jehwju1024@naver.com>
2026-04-14 16:39:01 +0900

    Revert "feat[front]: versionCode upgrade 3->4"
    This reverts commit 8ec9cebea9bdfcd52cbffc85e99cecbe4658e874.

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.swyp.brife"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
