bd9cbc7ba135eb05a2dfa47f33087ed1dcb22ab0
효정 <jehwju1024@naver.com>
2026-06-24 15:54:31 +0900

    chore[front]: Firebase 기본 설정 및 SDK 연동
    - google-services.json 연동
    - Google Services Gradle Plugin 추가
    - Firebase BoM 적용
    - Firebase Messaging 의존성 추가
    - Firebase Analytics 의존성 추가
    - Firebase SDK 사용을 위한 Gradle 설정 구성

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -71,6 +72,9 @@ dependencies {
     implementation("androidx.credentials:credentials-play-services-auth:1.6.0-rc02")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.2.0")
     implementation("com.google.android.gms:play-services-auth:21.3.0")
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.messaging)
+    implementation(libs.firebase.analytics)
 
     // Glance (홈 화면 위젯)
     implementation("androidx.glance:glance-appwidget:1.1.1")
